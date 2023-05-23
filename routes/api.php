6a384151464d3d1790c5c242775fa040f7bd39dd
Zhasulan-123 <zhas-88@bk.ru>
2023-05-24 02:22:32 +0600

    Cart product rest-api laravel

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\AdminController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +21,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public routes Categories
+Route::get('/categories', [CategoriesController::class, 'index']);
+
+// Public routes Products all
+Route::get('/products', [ProductsController::class, 'index']);
+
+// Public routes Login Register Users
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+// Public routes Login Register Admin
+Route::post('/admin/register', [AdminController::class, 'register']);
+Route::post('/admin/login', [AdminController::class, 'login']);
+
+// Protected routes
+Route::group(['middleware' => 'auth:sanctum'], function(){
+
+    // Create Order
+    Route::post('/order', [OrdersController::class, 'create']);
+
+    // All Order
+    Route::get('/order', [OrdersController::class, 'index']);
+
+    // Logout admin user
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
